4de127fc146b0512cbf17245178777a2b4543f7a
Avinash <avinashvkushwaha@outlook.com>
2024-06-28 23:46:49 +0530

    Added PostInstall Script

---
+++ Arch/PostInstall.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Open /etc/pacman.conf for editing and uncomment multilib section
+sudo sed -i "/\[multilib\]/,/Include/"' s/^#//' /etc/pacman.conf    
+echo "Multilib repository enabled!"
+
+# Update package database to include multilib
+sudo pacman -Syu --noconfirm
+
+# Install KDE Plasma Desktop and additional packages
+echo "Installing KDE Plasma Desktop and additional packages..."
+sudo pacman -S --noconfirm plasma-meta plasma-workspace ark konsole dolphin egl-wayland
+
+# Install display manager (SDDM)
+echo "Installing SDDM display manager..."
+sudo pacman -S --noconfirm sddm
+
+# Enable and start display manager
+echo "Enabling and starting SDDM..."
+sudo systemctl enable sddm.service
+sudo systemctl start sddm.service
+
+echo "KDE Plasma Desktop installation completed!"
+
+# Install necessary packages for yay
+sudo pacman -S --noconfirm curl zsh pacman-contrib git base-devel firefox vim vlc 
+
+# Download yay from AUR
+echo "Downloading yay from AUR..."
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+
+yay -S --noconfirm google-chrome visual-studio-code-bin postman-bin mongodb-compass
+
+# Prompt user for NVIDIA driver installation
+read -p "Do you want to install NVIDIA driver (y/n)? " choice
+if [[ $choice == [Yy] ]]; then
+    # Install NVIDIA driver and related packages
+    echo "Installing NVIDIA driver and utilities..."
+    yay -S --noconfirm nvidia nvidia-utils nvtop
+else
+    echo "Skipping NVIDIA driver installation."
+fi
+
+# Prompt user for Steam installation
+read -p "Do you want to install Steam? (y/n): " install_steam
+if [[ $install_steam =~ ^[Yy]$ ]]; then
+    # Install Steam from multilib repository
+    echo "Installing Steam..."
+    sudo pacman -S --noconfirm steam
+else
+    echo "Skipping Steam installation."
+fi
+
+# Installing ohmyzsh
+
+sudo pacman -S curl
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
